fbde1649fe10fc419e231260eae78fdb15dac740
Sudarshan <amoghsudarshan@gmail.com>
2025-05-24 12:42:52 +0530

    added SGD, momentum and adam separately in main.py, tested them using mnist dataset.

--- vanilla_neural_networks/main.py
+++ vanilla_neural_networks/main.py
@@ -2,6 +2,7 @@ import typing
 import time
 import numpy as np
 
+
 """
 To be implemented
 
@@ -17,12 +18,6 @@ Batch Normalization?
 """
 
 
-def sparse_categorical_cross_entropy_loss(y_predicted, y_true):
-    assert y_predicted.shape == y_true.shape, f"Found shape, y_predicted: {y_predicted.shape}, y_true:{y_true.shape}"
-
-    n, m = y_true.shape #the output units, batch_size respectively
-
-    return -np.sum(y_true * np.log(y_predicted+1e-9))/m
 
 
 def softmax(z):
@@ -31,12 +26,10 @@ def softmax(z):
     return temp/np.sum(temp, axis=0, keepdims=True) #sum along columns
     #assuming z.shape = (units, batch_size)
 
-
 def relu(x):
     return np.maximum(0, x)
 
 def sigmoid(x):
-
     return 1/(1+np.exp(-x))
 
 def binary_cross_entropy_loss(y_predicted, y_true):
@@ -44,6 +37,38 @@ def binary_cross_entropy_loss(y_predicted, y_true):
 
 def mean_squared_loss(y_predicted, y_true):
     return np.mean(np.square(y_predicted-y_true))
+
+def sparse_categorical_cross_entropy_loss(y_predicted, y_true):
+    assert y_predicted.shape == y_true.shape, f"Found shape, y_predicted: {y_predicted.shape}, y_true:{y_true.shape}"
+    n, m = y_true.shape #the output units, batch_size respectively
+    
+    return -np.sum(y_true * np.log(y_predicted+1e-9))/m
+
+#these are ONLY supported for binary classification. their behaviour on multi class classficiation is undefined
+def calculate_precision(y_predicted, y_true):
+
+    assert y_predicted.shape==y_true.shape, f"expected shapes to be same, found, y_predicted.shape={y_predicted.shape}, y_true.shape={y_true.shape}"
+
+    TP = np.sum((y_predicted==True) & (y_true==True))
+    FP = np.sum((y_predicted==True) & (y_true==False))
+
+    return (TP)/(TP+FP)
+
+def calculate_recall(y_predicted, y_true):
+
+    assert y_predicted.shape==y_true.shape, f"expected shapes to be same, found, y_predicted.shape={y_predicted.shape}, y_true.shape={y_true.shape}"
+
+    TP = np.sum((y_predicted==True) & (y_true==True))
+    FN = np.sum((y_predicted==False) & (y_true==False))
+
+    return (TP)/(TP+FN)
+
+def calculate_f1(y_predicted, y_true):
+
+    R = calculate_recall(y_predicted, y_true)
+    P = calculate_precision(y_predicted, y_true)
+
+    return 2*R*P/(R+P)
 
 def accuracy(y_predicted, y_true):
 
@@ -72,6 +97,73 @@ def calculate_loss(A, y, loss_type):
     else:
         raise NotImplementedError(f"Didn't implement loss type: {loss_type}")
 
+
+
+class Adam:
+
+    def __init__(self, network, learning_rate=1e-3, beta1=0.9, beta2=0.999):
+        
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.iteration=1
+        self.learning_rate=learning_rate
+        
+        for layer in network.layers:
+            layer.Sdw = layer.Vdw = np.zeros_like(layer.W)
+            layer.Sdb =layer.Vdb = np.zeros_like(layer.b)
+
+    def gradient_descent(self, network):
+
+        for layer in network.layers:
+
+            layer.Vdw = (self.beta1 * layer.Vdw + (1-self.beta1)*layer.dW)#/(1-self.beta1**self.iteration)
+            layer.Vdb = (self.beta1 * layer.Vdb + (1-self.beta1)*layer.db)#/(1-self.beta1**self.iteration)
+
+            layer.Sdw = (self.beta2 * layer.Sdw + (1-self.beta2)*np.square(layer.dW))#/(1-self.beta2**self.iteration)
+            layer.Sdb = (self.beta2 * layer.Sdb + (1-self.beta2)*np.square(layer.db))#/(1-self.beta2**self.iteration)
+
+            layer.W -= self.learning_rate* layer.Vdw / np.sqrt(layer.Sdw + 1e-9) * np.sqrt(1-self.beta2**self.iteration)/(1-self.beta1**self.iteration)
+            layer.b -= self.learning_rate* layer.Vdb / np.sqrt(layer.Sdb + 1e-9) * np.sqrt(1-self.beta2**self.iteration)/(1-self.beta1**self.iteration)
+
+        self.iteration+=1
+
+class Momentum:
+
+    def __init__(self, network, learning_rate=1e-3, beta=0.9):
+
+        if beta>=1:
+            raise ValueError(f"beta must be less than 1, found beta={beta}")
+
+        self.beta = beta
+        self.iteration = 1
+        self.learning_rate = learning_rate
+        for layer in network.layers:
+            layer.Vdw = np.zeros_like(layer.W)
+            layer.Vdb = np.zeros_like(layer.b)
+    
+    def gradient_descent(self, network):
+
+        for layer in network.layers:
+            layer.Vdw  = (self.beta*layer.Vdw + (1-self.beta)*layer.dW)#/(1-self.beta**self.iteration)
+            layer.Vdb  = (self.beta*layer.Vdb + (1-self.beta)*layer.db)#/(1-self.beta**self.iteration)
+            
+            layer.W -= self.learning_rate*layer.Vdw
+            layer.b -= self.learning_rate*layer.Vdb
+        
+        # self.iteration += 1
+
+class SGD:
+
+    def __init__(self, learning_rate=1e-3):
+        self.learning_rate = learning_rate
+
+    def gradient_descent(self, network):
+
+        for layer in network.layers:
+            layer.W -= self.learning_rate*layer.dW
+            layer.b -= self.learning_rate * layer.db
+        
+        
         
 
 
@@ -92,12 +184,19 @@ class InputLayer:
         pass
 
 
+
+
+
 class Dense:
     
     def __init__(self, units, activation='relu'):
         
         self.built=False #this is to track that the weights are not initialized
         self.units = units
+        
+        if activation not in ['relu', 'softmax', 'sigmoid']:
+            raise NotImplementedError(f"Activation type: {activation} not implemented")
+
         self.activation = activation
         self.W = None 
         self.b = None
@@ -107,7 +206,7 @@ class Dense:
         self.input_shape = input_shape
         # Used He/Xavier initialization (I don't remember the exact name)
         # Just to converge faster. could also give option for kernel_initializer
-        # like TF does, but I believe that much customization is not need
+        # like TF does, but I believe that much customization is not needed at start
         self.W = np.random.normal(0, np.sqrt(2/(input_shape+self.units)), (self.units, input_shape))
         self.b = np.zeros((self.units, 1))
         self.built=True
@@ -174,14 +273,17 @@ class Sequential:
 
     def __init__(self, layers:typing.List[typing.Union[Dense]]):
         self.layers=layers
+        self.loss = None
 
-    def compile(self, loss='mse'):
+    def compile(self, loss, optimizer=SGD()):
         # self.learning_rate = learning_rate
         # self.batch_size = batch_size
         # self.epochs=epochs
         self.loss = loss
+        self.optimizer = optimizer
 
-        
+    def add_layer(self, layer):
+        self.layers.append(layer)
 
     
     def build(self, input_size):
@@ -198,7 +300,7 @@ class Sequential:
             self.layers[i+1].build(self.layers[i].units)
             self.layers[i+1].built=True
     
-    def fit(self, X, y, batch_size=32, epochs=10, learning_rate=1e-3, metrics=[], random_state=42, X_test=None, y_test=None):
+    def fit(self, X, y, batch_size=32, epochs=10, metrics=[], random_state=None, X_test=None, y_test=None):
 
         if (X.shape[0]!=y.shape[0]):
             raise ValueError(f"Training rows and labelled rows not same. X.shape[0]={X.shape[0]}, y.shape={y.shape}")        
@@ -210,13 +312,9 @@ class Sequential:
         if (X_test is not None and y_test is not None) and (X_test.shape[0]!=y_test.shape[0]):
             raise ValueError(f"number of rows in X_test and y_test not same. X_test.shape={X_test.shape}, y_test.shape={y_test.shape}")
         
-        if (X_test is not None):
-            X_test = X_test.T
-            y_test=y_test.T
-        
-        
-        
-        
+
+        if random_state is not None:
+            np.random.set_state(random_state)        
         # if the weights are not initialized, this initializes them
         if not self.layers[0].built:
             self.build(X.shape[1])
@@ -228,6 +326,11 @@ class Sequential:
         # and number of columns = batch_size
         X = X.T
         y = y.T
+        
+        if (X_test is not None): #take transpose of test set, similar to training set
+            X_test = X_test.T
+            y_test=y_test.T        
+
 
         N, M = X.shape #features, training examples
 
@@ -236,6 +339,7 @@ class Sequential:
         test_loss = []
         train_loss = []
 
+        
 
         for epoch_num in range(epochs):
 
@@ -288,9 +392,11 @@ class Sequential:
                 for i in range(len(self.layers)):
                     dA = self.layers[len(self.layers)-i-1].back_prop(dA)
 
-                for layer in self.layers:
-                    layer.W -= learning_rate*layer.dW
-                    layer.b -= learning_rate*layer.db
+                self.optimizer.gradient_descent(self)
+
+                # for layer in self.layers:
+                #     layer.W -= learning_rate*layer.dW
+                #     layer.b -= learning_rate*layer.db
             
             train_loss.append(np.mean(epoch_training_loss_))
             train_accuracy.append(np.mean(epoch_training_accuracy_))
@@ -331,3 +437,5 @@ class Sequential:
 
         return X.T #output (examples, number_of_output)
         
+
+
